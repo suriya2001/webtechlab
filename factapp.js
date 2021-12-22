4169764992da9b594794f006f6519b9d0bef76ca
suriya2001 <suriyamanian020@gmail.com>
2021-12-22 09:27:49 +0530

    angular app

---
+++ factapp.js
@@ -0,0 +1,79 @@
+(function () {
+    'use strict';
+    
+    angular.module('ControllerAsApp', [])
+    
+    .controller('ShoppingListController2', ShoppingListController2)
+    .factory('ShoppingListFactory', ShoppingListFactory);
+
+    
+    
+    
+    // LIST #2 - controller
+    ShoppingListController2.$inject = ['ShoppingListFactory'];
+    function ShoppingListController2(ShoppingListFactory) {
+      var list2 = this;
+    
+      // Use factory to create new shopping list service
+      var shoppingList = ShoppingListFactory(3);
+    
+      list2.items = shoppingList.getItems();
+    
+      list2.itemName = "";
+      list2.itemQuantity = "";
+    
+      list2.addItem = function () {
+        try {
+          shoppingList.addItem(list2.itemName, list2.itemQuantity);
+        } catch (error) {
+          list2.errorMessage = error.message;
+        }
+    
+      }
+    
+      list2.removeItem = function (itemIndex) {
+        shoppingList.removeItem(itemIndex);
+      };
+    }
+    
+    
+    // If not specified, maxItems assumed unlimited
+    function ShoppingListService(maxItems) {
+      var service = this;
+    
+      // List of shopping items
+      var items = [];
+    
+      service.addItem = function (itemName, quantity) {
+        if ((maxItems === undefined) ||
+            (maxItems !== undefined) && (items.length < maxItems)) {
+          var item = {
+            name: itemName,
+            quantity: quantity
+          };
+          items.push(item);
+        }
+        else {
+          throw new Error("Max items (" + maxItems + ") reached.");
+        }
+      };
+    
+      service.removeItem = function (itemIndex) {
+        items.splice(itemIndex, 1);
+      };
+    
+      service.getItems = function () {
+        return items;
+      };
+    }
+    
+    
+    function ShoppingListFactory() {
+      var factory = function (maxItems) {
+        return new ShoppingListService(maxItems);
+      };
+    
+      return factory;
+    }
+    
+    })();
